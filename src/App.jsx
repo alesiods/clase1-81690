df2b2d593549fd360a45d405fe5ed1d552d0e2eb
alesiods <alesioadrover@gmail.com>
2025-10-20 22:25:24 -0300

    primer clase

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import Boton from './Boton/Boton'
+
+
+const App = () => {
+
+
+  return (
+    <>
+      <h1>Clase 1 de React</h1>
+      <h2>subitutlo</h2>
+      <Boton/>
+      <Boton/>
+      <Boton/>
+      <Boton/>
+      <Boton/>
+      <Boton/>
+      <Boton/>
+    </>
+
+    
+  )
+}
+
+export default App
